0ad6ac3127732aede090501fff429813c376c1e6
versteey <lolear@live.nl>
2014-11-25 16:49:01 +0100

    final release

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     private Scanner scan =  new Scanner(System.in);
+
     public static void main(String[] args){
         Main main = new Main();
         Player player =  new Player(true);
