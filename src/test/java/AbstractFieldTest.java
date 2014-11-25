bf83e50269d084a851276fb22e22fa4dda97048a
versteey <lolear@live.nl>
2014-11-25 15:34:13 +0100

    Update AbstractFieldTest.java

--- src/test/java/AbstractFieldTest.java
+++ src/test/java/AbstractFieldTest.java
@@ -1,8 +1,7 @@
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class AbstractFieldTest extends TestCase {
     public void test() {
-        Assert.assertTrue(true);
+        assertTrue(true);
     }
 }
